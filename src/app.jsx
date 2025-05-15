5e1b8477ebc3089af344a87134e54b7afa7da250
sagarhasan273 <sagarhasan273@gmail.com>
2025-05-16 00:13:52 +0600

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,43 @@
+import 'src/global.css';
+
+// ----------------------------------------------------------------------
+
+import { Router } from 'src/routes/sections';
+
+import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
+
+import { LocalizationProvider } from 'src/locales';
+import { I18nProvider } from 'src/locales/i18n-provider';
+import { ThemeProvider } from 'src/theme/theme-provider';
+
+import { Snackbar } from 'src/components/snackbar';
+import { ProgressBar } from 'src/components/progress-bar';
+import { MotionLazy } from 'src/components/animate/motion-lazy';
+import { SettingsDrawer, defaultSettings, SettingsProvider } from 'src/components/settings';
+
+import { CheckoutProvider } from 'src/sections/checkout/context';
+
+// ----------------------------------------------------------------------
+
+export default function App() {
+  useScrollToTop();
+
+  return (
+    <I18nProvider>
+      <LocalizationProvider>
+        <SettingsProvider settings={defaultSettings}>
+          <ThemeProvider>
+            <MotionLazy>
+              <CheckoutProvider>
+                <Snackbar />
+                <ProgressBar />
+                <SettingsDrawer />
+                <Router />
+              </CheckoutProvider>
+            </MotionLazy>
+          </ThemeProvider>
+        </SettingsProvider>
+      </LocalizationProvider>
+    </I18nProvider>
+  );
+}
